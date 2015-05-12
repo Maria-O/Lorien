4a3f4f89ff37df5e3fe8999c9887aebb9e5599a6
Maria-O <1mariaoboznaya1@gmail.com>
2015-05-12 12:37:26 +0300

    charts task has been refactored

--- Mariia/Practice/10 D3&Highcharts/js/script.js
+++ Mariia/Practice/10 D3&Highcharts/js/script.js
@@ -10,7 +10,6 @@ var menuBtn = $("#menu-btn"),
 
 menuBtn.on('click', function(){
     asideNav.toggleClass('hidden');
-
 });
 
 asideNavItem.on('click', function() {
@@ -23,12 +22,19 @@ topNavItem.on('click', function(){
     $(this).addClass('active');
 });
 
-//field.on('focus', function(){
-//    $(this).parent().addClass('focused-holder');
-//    findBtn.show();
-//});
-//
-//field.on('blur', function(){
-//    $(this).parent().removeClass('focused-holder');
-//    findBtn.hide();
-//});
+field.on('focus', function(){
+    var field = $(this);
+    field.closest('.header-holder-item').addClass('focused-holder');
+    if (field.attr('type')=="search") {
+        findBtn.show();
+    }
+});
+
+field.on('blur', function() {
+    var field = $(this);
+    field.closest('.header-holder-item').removeClass('focused-holder');
+    if (field.attr('type') == "search") {
+        findBtn.hide();
+        field.val("");
+    }
+});
